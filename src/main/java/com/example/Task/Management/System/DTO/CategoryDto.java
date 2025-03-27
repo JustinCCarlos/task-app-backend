f143069e14c06dc5de3bbd89c645ff7b702801e9
JustinCCarlos <justincharles.carlos@gmail.com>
2025-03-27 23:06:01 +0800

    chore: Update database dependencies and refactor tests
    - Changed database source
    - Updated mysql-connector-j from 8.3.0 to 9.2.0
    - Updated tomcat-embed-core to address vulnerability
    - Updated CategoryDto and Controller test

--- src/main/java/com/example/Task/Management/System/DTO/CategoryDto.java
+++ src/main/java/com/example/Task/Management/System/DTO/CategoryDto.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Builder(toBuilder = true)
 public class CategoryDto {
     private Long categoryId;
 
--- src/test/java/com/example/Task/Management/System/controller/CategoryControllerTest.java
+++ src/test/java/com/example/Task/Management/System/controller/CategoryControllerTest.java
@@ -4,6 +4,7 @@ import com.example.Task.Management.System.DTO.CategoryDto;
 import com.example.Task.Management.System.Service.CategoryService;
 import com.example.Task.Management.System.controllers.CategoryController;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -37,39 +38,43 @@ public class CategoryControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private CategoryDto defaultCategoryDto;
+
+    @BeforeEach
+    public void init() {
+        defaultCategoryDto = CategoryDto.builder()
+                .categoryId(1L)
+                .name("Default CategoryDto")
+                .build();
+    }
+
     @Test
     public void CategoryController_CreateCategory_ReturnCreated() throws Exception{
-        CategoryDto categoryDto = CategoryDto.builder()
-                .categoryId(1L)
-                .name("Test categoryDto")
-                .build();
-
-        when(categoryService.createCategory(any(CategoryDto.class))).thenReturn(categoryDto);
+        when(categoryService.createCategory(any(CategoryDto.class))).thenReturn(defaultCategoryDto);
 
         mockMvc.perform(post("/api/categories")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(categoryDto)))
+                .content(objectMapper.writeValueAsString(defaultCategoryDto)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.category_id").value(1))
-                .andExpect(jsonPath("$.name").value("Test categoryDto"));
+                .andExpect(jsonPath("$.categoryId").value(1L))
+                .andExpect(jsonPath("$.name").value("Default CategoryDto"))
+                .andReturn();
 
         verify(categoryService).createCategory(any(CategoryDto.class));
     }
 
     @Test
     public void CategoryController_UpdateCategory_ReturnUpdated() throws Exception {
-        CategoryDto newCategoryDto = CategoryDto.builder()
-                .categoryId(1L)
-                .name("New categoryDto")
-                .build();
+        Long categoryId = 1L;
+        CategoryDto updatedCategoryDto = defaultCategoryDto.toBuilder().name("Updated CategoryDto").build();
 
-        when(categoryService.updateCategory(anyLong(), any(CategoryDto.class))).thenReturn(newCategoryDto);
+        when(categoryService.updateCategory(eq(categoryId), any(CategoryDto.class))).thenReturn(updatedCategoryDto);
 
         mockMvc.perform(put("/api/categories/{id}", 1L)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(newCategoryDto)))
-                .andExpect(jsonPath("$.category_id").value(1L))
-                .andExpect(jsonPath("$.name").value("New categoryDto"))
+                .content(objectMapper.writeValueAsString(updatedCategoryDto)))
+                .andExpect(jsonPath("$.categoryId").value(1L))
+                .andExpect(jsonPath("$.name").value("Updated CategoryDto"))
                 .andExpect(status().isOk());
 
         verify(categoryService).updateCategory(eq(1L), any(CategoryDto.class));
